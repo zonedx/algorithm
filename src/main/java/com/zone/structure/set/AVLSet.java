a4950b701c32e86415ba56648e3f97bd716cfbe7
xin.duan <xin.duan@cygia.com>
2019-12-05 14:20:35 +0800

    avl

---
+++ src/main/java/com/zone/structure/set/AVLSet.java
@@ -0,0 +1,41 @@
+package com.zone.structure.set;
+
+import com.zone.structure.bst.AVLTree;
+
+/**
+ * @ClassName: AVLSet
+ * @Date 2019-12-05 14:06
+ * @Author duanxin
+ **/
+public class AVLSet<E extends Comparable<E>> implements Set<E> {
+
+    private AVLTree<E,Object> avl;
+
+    public AVLSet(){
+        avl = new AVLTree<>();
+    }
+    @Override
+    public void add(E e) {
+        avl.add(e,null);
+    }
+
+    @Override
+    public void remove(E e) {
+        avl.remove(e);
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return avl.contains(e);
+    }
+
+    @Override
+    public int getSize() {
+        return avl.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return avl.isEmpty();
+    }
+}
